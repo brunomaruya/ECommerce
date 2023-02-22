1c4493c387b889f7be44106a8307edaab239ac9a
Bruno <brun0yuj1ym02@gmail.com>
2023-02-22 20:33:42 +0900

    start using next auth installed packages, update nextconfig, create config folder, create dbConnext.ts file

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    DB_URI: 'mongobd://localhost:27017/next13-auth',
+    NEXTAUTH_SECRET: 'codingwithabbas',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
